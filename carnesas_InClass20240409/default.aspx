b447ec29094a70f19de664116056e0f883773c50
ascarnes <carnesas@mail.uc.edu>
2024-04-11 11:44:53 -0400

    Add project files.

---
+++ carnesas_InClass20240409/default.aspx
@@ -0,0 +1,36 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="default.aspx.cs" Inherits="carnesas_InClass20240409._default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+   <style type="text/css">
+.ListBoxCssClass
+{
+color:GhostWhite;
+background-color:DarkOliveGreen;
+font-family:Courier New;
+font-size:large;
+font-style:italic;
+}
+</style>
+</head>
+
+
+<body>
+    <form id="form1" runat="server">
+        <asp:ListBox OnSelectedIndexChanged="lbFruits_SelectedIndexChanged" ID="lbFruits" runat="server" CssClass="ListBoxCssClass" AutoPostBack="True" DataSourceID="SqlDataSourceInClass20240409">
+                <asp:ListItem>Peach</asp:ListItem>
+                <asp:ListItem>Plum</asp:ListItem>
+                <asp:ListItem>Apple</asp:ListItem>
+                <asp:ListItem>Guava</asp:ListItem>
+                  <asp:ListItem>Avocado</asp:ListItem>
+            </asp:ListBox>        
+        <div>
+
+        </div>
+        <asp:Label ID="lblSelectedFruit" runat="server" Text="Select a Fruit"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ carnesas_InClass20240409/default.aspx.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace carnesas_InClass20240409
+{
+    public partial class _default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                // Your existing initialization logic here
+
+                // Add "Banana" to the ListBox
+                lbFruits.Items.Add("Banana");
+            }
+        }
+        protected void lbFruits_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            // Get the selected fruit from the ListBox
+            string selectedFruit = lbFruits.SelectedItem.Text;
+
+            // Update the label's text with the selected fruit
+            lblSelectedFruit.Text = $"{selectedFruit}";
+        }
+
+        protected void SqlDataSourceInClass20240409_Selecting(object sender, SqlDataSourceSelectingEventArgs e)
+        {
+
+        }
+    }
+}
+        //I need an event that will run when the user, in their browser, selects a fruit from the list box
+        //"Event Driven programming"
+        //Our event is the user selecting an item from the list box
+    
